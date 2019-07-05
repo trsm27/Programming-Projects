d245b237ad074805ac5832fc32bf9cd72e0424d0
Mustaeen Siddiqui <minersatwork27@gmail.com>
2019-07-05 19:18:57 +0100

    added dice.py

---
+++ python/dice.py
@@ -0,0 +1,3 @@
+import random #imports random module
+possibilities = [1, 2, 3, 4, 5, 6] #makes list for all possibilities
+print(random.choice(possibilities)) #prints out a random element from the list called possibilities
